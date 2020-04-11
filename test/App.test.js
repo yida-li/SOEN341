2e9134106b2daa1afddf41984c348a9b6cd187fa
Ken Nguyen <kenn.tnguyen@gmail.com>
2020-04-10 23:40:34 -0400

    adding unit test

--- test/App.test.js
+++ test/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import App from './App';
+import App from "../src/App"
 
 it("render without crashing", ()=>{
   const div = document.createElement("div");
-  ReactDOM.render(<App/>,div)
+  ReactDOM.render(<App/>)
 })
--- test/NavBar.test.js
+++ test/NavBar.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import App from './App';
+import NavBar from "../src/components/layout/Navbar"
 
 it("render without crashing", ()=>{
   const div = document.createElement("div");
-  ReactDOM.render(<App/>,div)
+  ReactDOM.render(<NavBar/>)
 })
